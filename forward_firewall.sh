fdb5856001044886c02bb278beeca10f97f235f0
Giovanni Torsello <giovanni.torsello@gmail.com>
2021-03-01 20:42:40 +0100

    Script Firewall

--- forward_firewall.sh
+++ forward_firewall.sh
@@ -1,5 +1,5 @@
 ipfrontend="10.1.1.5"
-ipvideobridge="10.1.1.5"
+ipvideobridge="10.1.1.4"
 /usr/sbin/iptables -t nat -F
 /usr/sbin/iptables -t nat -A PREROUTING -p tcp --dport 80    -d 172.16.200.104 -j DNAT --to $ipfrontend:80
 /usr/sbin/iptables -t nat -A PREROUTING -p tcp --dport 443   -d 172.16.200.104 -j DNAT --to $ipfrontend:443
